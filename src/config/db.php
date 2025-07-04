1a409cb68339aec8a5b53dfde8f84eb7b1009fd5
iOnilec <c.onilec.0t@gmail.com>
2025-07-04 16:06:41 -0300

    feat: composer on dockerfile php

---
+++ src/config/db.php
@@ -0,0 +1,25 @@
+<?php
+
+if (!function_exists('connect')) {
+    function connect()
+    {
+        $host = 'localhost';
+        $port = '5432';
+        $dbname = 'api_companies';
+        $user = 'admin';
+        $password = 'admin';
+
+        try {
+            $pdo = new PDO("pgsql:host=$host;port=$port;dbname=$dbname", $user, $password);
+
+            $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+
+            return $pdo;
+        } catch (Exception $e) {
+
+            die("Erro na conexão com o banco de dados \n" . $e->getMessage());
+        }
+    }
+}
+
+connect();
